cd7cb37add05801317bb091cf7ae09d32e1d6732
MatheusVict <matheusvictorhenrique@gmail.com>
2023-02-03 12:33:47 -0300

    refact whatsapp component

--- client/src/components/whatsapp/index.jsx
+++ client/src/components/whatsapp/index.jsx
@@ -1,28 +1,77 @@
-import { GrCircleAlert } from "react-icons/gr";
-import { RightContent, ServerContainer, ServerStatus } from "./style";
+import { 
+    LeftContent, 
+    LoggoutButton, 
+    RightContent, 
+    ServerContainer, 
+    ServerStatusOff, 
+    ServerStatusOn, 
+    WhatsAppNumber 
+} from "./style";
 import Menu from "../menu";
 import { useParams } from "react-router";
+import { useEffect, useState } from "react";
 
 function WhatsApp() {
-    const {userIns} = useParams();
+    const [status, setStatus] = useState(true); // Status se o whats está conectado
+    const [whatsappConnected, setWhatsappConnected] = useState(556283214457); // Número conectado
+    const {userIns} = useParams(); //Informção do usuário para busca no banco
+
+    useEffect(() => {
+        /* 
+        Verifica na API se está conectado
+        Se estivar seta no state e renderiza condicionalmente
+        Vai usar todos os states acima para renderização condicional
+        */
+        
+    }, [])
 
     return (
         <>
         <Menu/>
             <ServerContainer>
-                Status do servidor:
-                <ServerStatus>
-                    <p className='status'>Ativo</p>
-                </ServerStatus>
+                <LeftContent>
+                   <p>Status do servidor:</p>
+                {status ? (
+                    <ServerStatusOn>
+                        <p className='status'>Ativo</p>
+                    </ServerStatusOn>
+                ): <ServerStatusOff>
+                        <p className='status'>Desativado</p>
+                    </ServerStatusOff>}
+                </LeftContent>
+                
                 <RightContent>
-                    <p>
-                    Parabéns! Seu WhatsApp conectou com sucesso! <br />
-                    Agora, você pode até desligar seu celular e computador <br /> 
-                    que o bot continuará funcionando. <br />
-                    Acesse seu WhatsApp pelo celular no mínimo a cada 14 dias <br />
-                    para confirmação da posse do número. <br />
-                    </p>
-                    <GrCircleAlert className='icon' />
+                    {status ? (
+                        <>
+                            <p>
+                                Parabéns! Seu WhatsApp conectou com sucesso! <br />
+                                Agora, você pode até desligar seu celular e computador <br /> 
+                                que o bot continuará funcionando. <br />
+                                Acesse seu WhatsApp pelo celular no mínimo a cada 14 dias <br />
+                                para confirmação da posse do número.
+                            </p>
+                            <br />
+                            <p className="whats_phone">
+                                WhatsApp conectado:
+                                </p>
+                            <WhatsAppNumber>
+                                <p className="whats_phone">{whatsappConnected}</p>
+                            </WhatsAppNumber>
+                            <LoggoutButton>
+                                Sair
+                            </LoggoutButton>
+                        </>
+                    ): (
+                        <>
+                            <p>
+                                Whatsapp não conectado
+                            </p>
+                            <br />
+                            <LoggoutButton>
+                                Conectar
+                            </LoggoutButton>
+                        </>
+                    )}
                 </RightContent>
             </ServerContainer>
         </>
--- client/src/components/whatsapp/style.js
+++ client/src/components/whatsapp/style.js
@@ -3,12 +3,17 @@ import styled from "styled-components";
 export const ServerContainer = styled.div`
 margin-top: 25px;
 max-width: 100%;
-position: left;
-display: wrap;
+display: flex;
+gap: 40%; 
+    p {
+        padding-left: 9px;
+    }
 `
-export const ServerStatus = styled.div`
+export const ServerStatusOn = styled.div`
+margin-top: 15px;
+margin-left: 15px;
 text-align: center;
-max-width: 30%;
+max-width: 120px;
 border-radius: 5px;
 height: 25px;
 border: 1px solid #97e9b6;
@@ -16,12 +21,51 @@ border: 1px solid #97e9b6;
         color: #97e9b6;
     }
 `
+export const ServerStatusOff = styled.div`
+margin-top: 15px;
+margin-left: 15px;
+text-align: center;
+max-width: 120px;
+border-radius: 5px;
+height: 25px;
+border: 1px solid #ec2300;
+    .status {
+        color: #ec2300;
+    }
+`
+
+export const LoggoutButton = styled.button`
+background-color: #FFF;
+border: 1px solid #b7a8ef;
+color: #b7a8ef;
+width: 120px;
+height: 36px;
+margin-top: 25px;
+border-radius: 5px;
+:hover {
+    background-color: #b7a8ef;
+    color: #FFF;
+    cursor: pointer;
+}
+`
+
+export const WhatsAppNumber = styled.div`
+width: 403;
+height: 46px;
+background-color: #f5f5f5;
+text-align: center;
+color: #c0c0c0;
+border: 1px solid #eeeeee;
+border-radius: 5px;
+    .whats_phone {
+        margin-top: 12px;
+    }
+`
+
+export const LeftContent = styled.div`
+flex-diraction: column;
+`
 
 export const RightContent = styled.div`
-position: right;
-display: flex;
-margin-left: 52%;
-    .icon {
-        height: 100px;
-    }
+    
 `
